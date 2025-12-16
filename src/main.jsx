ed309c264bed04ff523f8146a4967921f793a107
DanielBasco <danielsgeniale@gmail.com>
2025-12-16 17:04:15 +0100

    Tilføjet home og login endpoint og routes setup i main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 import App from './layout/App.jsx'
+import Home from "./pages/Home/Home.jsx"
+import Login from "./pages/Login/Login.jsx"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<Home />}>
+
+  </Route>
+
+  <Route path="login" element={<Login />}></Route>
+</Routes>
+</BrowserRouter>
 )
